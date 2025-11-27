1f9c58f98d17c907b65424e7be436dc1fb47d924
다현 <123882512+dahyun24@users.noreply.github.com>
2025-11-27 09:57:45 +0900

    #3 feat: clientstreaming 클라이언트 구현

--- lec-07-prg-03-clientstreaming/src/main/java/com/example/grpc/client.java
+++ lec-07-prg-03-clientstreaming/src/main/java/com/example/grpc/client.java
@@ -1,4 +1,61 @@
 package com.example.grpc;
 
+
+import clientstreaming.ClientStreamingGrpc;
+import clientstreaming.Message;
+import clientstreaming.Number;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+
 public class client {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ManagedChannel channel = ManagedChannelBuilder
+                .forAddress("localhost", 50051)
+                .usePlaintext()
+                .build();
+
+        ClientStreamingGrpc.ClientStreamingStub asyncStub =
+                ClientStreamingGrpc.newStub(channel);
+
+        StreamObserver<Message> requestObserver =
+                asyncStub.getServerResponse(new StreamObserver<Number>() {
+
+                    @Override
+                    public void onNext(Number number) {
+                        System.out.println("[server to client] " + number.getValue());
+                    }
+
+                    @Override
+                    public void onError(Throwable throwable) {
+                    }
+
+                    @Override
+                    public void onCompleted() {
+                    }
+                });
+
+        // Python generate_messages()
+        String[] msgs = {
+                "message #1", "message #2", "message #3", "message #4", "message #5"
+        };
+
+        for (String m : msgs) {
+            System.out.println("[client to server] " + m);
+            Message msg = Message.newBuilder()
+                    .setMessage(m)
+                    .build();
+            requestObserver.onNext(msg);
+            Thread.sleep(200);
+        }
+
+        requestObserver.onCompleted();
+
+        Thread.sleep(1000);
+
+        channel.shutdown();
+    }
 }
+
